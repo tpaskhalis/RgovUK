a12798ed79fa1c62369cb22a5a15cb938d8c2838
Tom Paskhalis <tpaskhalis@gmail.com>
2018-03-19 19:09:25 -0400

    Make driver creation exportable

--- R/driver.R
+++ R/driver.R
@@ -1,4 +1,24 @@
-create_driver <- function(port = 4567L,
+# create environment for Selenium driver
+.govenv <- new.env()
+
+#' Start Selenium driver 
+#' 
+#' \code{start_browser} should be run before execution of other functions in the
+#' package. Selenium driver is required for both application of filters and 
+#' parsing results. Further documentation is available in \code{\link[RSelenium]{rsDriver}}.
+#' 
+#' @param port Port to use
+#' @param browser Which browser should be used
+#' \describe{
+#' \item{\code{"chrome"}}{}
+#' \item{\code{"firefox"}}{}
+#' \item{\code{"phantomjs"}}{}
+#' \item{\code{"internet explorer"}}{}
+#' }
+#' @param verbose Print out messages
+#' 
+#' @export
+start_browser <- function(port = 4567L,
                           browser = c("chrome",
                                       "firefox",
                                       "phantomjs",
@@ -7,6 +27,17 @@ create_driver <- function(port = 4567L,
   browser <- match.arg(browser)
   
   driver <- RSelenium::rsDriver(port = port, browser = browser, verbose = verbose)
-  driver <- driver[["client"]]
-  driver
+  assign("driver", driver[["client"]], envir = .govenv)
+}
+
+#' Open main page of gov.UK publications
+#' \url{https://www.gov.uk/government/publications}
+#' 
+#' @export
+main_page <- function() {
+  if (!exists("driver", where = .govenv)) {
+    stop("Browser instance could not be found. Try, running start_browser() first")
+  }
+  url <- paste0(BASEURL, PUBLICATIONS)
+  .govenv$driver$navigate(url)
 }
