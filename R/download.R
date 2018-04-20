6f660d5a87df1ce47f98c480b216ae334a4b43d2
Tom Paskhalis <tpaskhalis@gmail.com>
2018-04-19 20:58:35 -0400

    Add limits, examples

--- R/download.R
+++ R/download.R
@@ -1,26 +1,52 @@
-#' Download files published on gov.uk publications 
-#' \url{https://www.gov.uk/government/publications}
+#' Download gov.UK publications
 #' 
+#' Download files published on
+#' \href{https://www.gov.uk/government/publications}{gov.uk publications}
+#'
 #' @param directory Specify directory for download
-#' @param type file type of downloaded documents. See \code{\link{download_pages}}
+#' @param limit Numeric value, setting the limit of the number of downloaded files,
+#' NULL (all files that meet the criteria) by default
+#' @param type File type of downloaded documents. See \code{\link{download_html}}
 #'  for retrieving page source.
 #' \describe{
 #' \item{all}{(default) do not pre-filter on file type}
-#' \item{csv}{}
-#' \item{pdf}{}
+#' \item{csv}{Download only CSV files}
+#' \item{pdf}{Download only PDF files}
 #' }
-#' 
+#'
+#' @examples
+#' \dontrun{
+#' download_files(tempdir(), limit = 10, type = "csv")
+#' }
+#'
+#' @author Tom Paskhalis
 #' @export
-download_files <- function(directory, type = c("all", "csv", "pdf")) {
+download_files <- function(directory,
+                           limit = NULL,
+                           type = c("all", "csv", "pdf")) {
   type <- match.arg(type)
   
   check_browser()
   
+  num <- 0
   repeat {
     src <- unlist(.govenv$driver$getPageSource())
     res <- parse_results(src)
     urls <- unlist(lapply(res, parse_links, type))
+    
+    if (!is.null(limit)) {
+      if ((num + length(urls)) >= limit) {
+      top <- num + limit
+      urls <- urls[seq(1, top)]
+    }
+    num <- num + length(urls)
+    }
+    
     invisible(lapply(urls, function(x) download(directory, x)))
+    
+    if (!is.null(limit) && num >= limit) {
+      break
+    }
     
     # check whether there are more pages
     nextpage <- get_next_page()
@@ -30,45 +56,76 @@ download_files <- function(directory, type = c("all", "csv", "pdf")) {
   }
 }
 
-#' Download the HTML source of pages published on gov.uk publications
-#' \url{https://www.gov.uk/government/publications}
-#' 
+#' Download the HTML source of pages published on
+#' \href{https://www.gov.uk/government/publications}{gov.uk publications}
+#'
+#' @param limit Numeric value, setting the limit of the number of downloaded pages,
+#' NULL (all pages that meet the criteria) by default
+#'
+#' @examples
+#' \dontrun{
+#' # download HTML source code of search results
+#' p <- download_html(limit = 10)
+#' }
+#'
+#' @author Tom Paskhalis
 #' @export
-download_pages <- function() {
+download_html <- function(limit = NULL) {
   check_browser()
   
+  num <- 0
+  html <- character()
   repeat {
     src <- unlist(.govenv$driver$getPageSource())
     urls <- parse_results(src)
     
-    # txts <- character()
-    # txts <- c(txts, vapply(res, RCurl::getURL, character(1)))
-    txts <- vapply(urls, RCurl::getURL, character(1))
+    if (!is.null(limit)) {
+      if ((num + length(urls)) >= limit) {
+        top <- num + limit
+        urls <- urls[seq(1, top)]
+      }
+      num <- num + length(urls)
+    }
+    
+    txts <- unlist(lapply(lapply(urls, httr::GET),
+                          httr::content, type = "text", encoding = "UTF-8"))
+    html <- c(html, txts)
+    # txts <- vapply(urls, RCurl::getURL, character(1))
+    
+    if (!is.null(limit) && num >= limit) {
+      break
+    }
     
     nextpage <- get_next_page()
     if (identical(nextpage, FALSE)) {
       break
     }
   }
-  txts
+  html
 }
 
 download <- function(directory, url) {
   # create directory if it doesn't exist
   if (!dir.exists(directory)) {
-    message(sprintf("Creating directory at %s", directory))
+    print(sprintf("Creating directory at %s", directory))
     dir.create(directory, recursive = TRUE)
   }
   
   path <- unlist(strsplit(url, "/"))
   filename <- path[length(path)]
   filepath <- file.path(directory, filename)
-  url <- paste0(BASEURL, url)
-  utils::download.file(url, filepath)
+  parsed <- httr::parse_url(url)
+  if (is.null(parsed$scheme) || parsed$scheme %in% c("https", "http", "ftp")) {
+    if (is.null(parsed$hostname)) {
+      url <- paste0(BASEURL, url)
+    }
+    curl::curl_download(url, filepath)
+  } else {
+    message(sprintf("The link %s is not a valid http link", url))
+  }
 }
 
 get_next_page <- function(src) {
-  # Could be implemented with an XML search
   xpath <- paste(c("//nav[@id='show-more-documents']",
                    "ul[@class='previous-n
                    ext-navigation']",
@@ -80,8 +137,7 @@ get_next_page <- function(src) {
   if (!inherits(nextpage, "error")) {
     nextpage$clickElement()
     TRUE
-  }
-  else {
+  } else {
     FALSE
   }
 }
@@ -105,7 +161,7 @@ parse_results <- function(src) {
 parse_links <- function(url, type = c("all", "csv", "pdf")) {
   type <- match.arg(type)
   
-  txt <- RCurl::getURL(url)
+  txt <- httr::content(httr::GET(url), type = "text", encoding = "UTF-8")
   body <- get_html_body(txt)
   xpath <- "//section[@class='attachment embedded']"
   attaches <- XML::getNodeSet(body, xpath)
@@ -115,13 +171,11 @@ parse_links <- function(url, type = c("all", "csv", "pdf")) {
     xpath <- paste(c("./div[@class='attachment-details']",
                    "a[contains(@href, '.csv') and not(contains(@href, 'preview'))]"),
                    collapse = "//")
-  }
-  else if (type == "pdf") {
+  } else if (type == "pdf") {
     xpath <- paste(c("./div[@class='attachment-details']",
                      "a[contains(@href, '.pdf') and not(contains(@href, 'preview'))]"),
                    collapse = "//")
-  }
-  else {
+  } else {
     xpath <- paste(c("./div[@class='attachment-details']",
                      "a[not(contains(@href, 'preview'))]"),
                    collapse = "//")
@@ -131,3 +185,11 @@ parse_links <- function(url, type = c("all", "csv", "pdf")) {
                                                      XML::xmlGetAttr, name = "href")))
   urls
 }
+
+#' @rdname download_files
+#' @export
+govuk_download_files <- download_files
+
+#' @rdname download_html
+#' @export
+govuk_download_html <- download_html
