1521f5bb5ae3526b79e8dd4da06d763c593f072f
Tom Paskhalis <tpaskhalis@gmail.com>
2018-04-19 21:01:37 -0400

    Add ports to avoid conflict

--- tests/testthat/test_driver.R
+++ tests/testthat/test_driver.R
@@ -2,7 +2,8 @@ context("Minimum functionality of the WebDriver")
 
 test_that("Browser (default, Chrome) can be initiated", {
   caps <- list(chromeOptions = list(args = c("--headless", "--disable-gpu")))
-  start_browser(docker = FALSE,
+  start_browser(port = 4441L,
+                docker = FALSE,
                 browser = "chrome",
                 verbose = FALSE,
                 check = TRUE,
@@ -13,7 +14,8 @@ test_that("Browser (default, Chrome) can be initiated", {
 })
 
 test_that("Firefox can be initiated", {
-  start_browser(docker = FALSE,
+  start_browser(port = 4442L,
+                docker = FALSE,
                 browser = "firefox",
                 verbose = TRUE,
                 check = TRUE)
@@ -22,35 +24,38 @@ test_that("Firefox can be initiated", {
   stop_browser()
 })
 
-# test_that("PhantomJS can be initiated", {
-#   start_browser(docker = FALSE,
-#                 browser = "phantomjs",
-#                 verbose = FALSE,
-#                 check = TRUE)
-#   expect_true(exists("driver", where = .govenv))
-#   expect_true(exists("server", where = .govenv))
-#   stop_browser()
-# })
+test_that("PhantomJS can be initiated", {
+  start_browser(port = 4443L,
+                docker = FALSE,
+                browser = "phantomjs",
+                verbose = FALSE,
+                check = TRUE)
+  expect_true(exists("driver", where = .govenv))
+  expect_true(exists("server", where = .govenv))
+  stop_browser()
+})
 
 test_that("Docker can be used", {
   id <- system2("docker",
                 args = c("run", "-d", "-p", "4445:4444 selenium/standalone-firefox:3.10.0"),
                 stdout = TRUE)
+  Sys.sleep(5)
   start_browser(port = 4445L, docker = TRUE)
   expect_true(exists("driver", where = .govenv))
-  system2("docker", args = c("kill", id), stdout = FALSE)
   stop_browser()
+  system2("docker", args = c("kill", id), stdout = FALSE)
 })
 
 test_that("Browser is able to navigate to the main page", {
   caps <- list(chromeOptions = list(args = c("--headless", "--disable-gpu")))
-  start_browser(docker = FALSE,
+  start_browser(port = 4446L,
+                docker = FALSE,
                 browser = "chrome",
                 verbose = FALSE,
                 check = TRUE,
                 extraCapabilities = caps)
- main_page()
- title <- unlist(get_title())
+ expect_silent(main_page())
+ title <- unlist(rgovuk:::get_title())
  expect_identical(title, "Publications - GOV.UK")
  stop_browser()
 })
